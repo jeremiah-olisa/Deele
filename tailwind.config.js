39a771ec4d81dd937b2ecad7a3adbcb0d96d5121
Jeremiah Olisa <31079573+jeremiah-olisa@users.noreply.github.com>
2024-08-24 12:23:35 +0100

    login page

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    theme: {
+        extend: {
+            colors: {
+                'crimson': 'hsla(354, 70%, 50%, 1)',
+                'steel-blue': 'hsla(207, 54%, 51%, 1)',
+                'carrot-orange': 'hsla(32, 90%, 59%, 1)',
+                'dark-purple': 'hsla(250, 50%, 12%, 1)',
+                'red-cmyk': 'hsla(1, 89%, 55%, 1)',
+            }
+        }
+    }
+}
