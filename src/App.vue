39132f4d3659b94adbdeb716778e619be921e60a
Jeremiah Olisa <31079573+jeremiah-olisa@users.noreply.github.com>
2024-09-08 18:50:30 +0100

    banner

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
+import BannerComponent from './components/Home/BannerComponent.vue'
 import NavigationBar from './components/layout/NavigationBar.vue'
 import PromotionHeader from './components/layout/PromotionHeader.vue'
 </script>
 
 <template>
   <PromotionHeader />
-  <section class="px-40 bg-gradient-to-br from-[#FFC94D] to-[#F9A825]">
+  <section class="px-4 sm:px-20 md:px-40 bg-gradient-to-br from-[#FFC94D] to-[#F9A825]">
     <NavigationBar />
+    <BannerComponent />
   </section>
 </template>
---
+++ src/components/Home/BannerComponent.vue
@@ -0,0 +1,34 @@
+<script setup></script>
+<template>
+  <div class="grid grid-cols-2 gap-4 justify-center items-center max-md:py-4">
+    <div class="max-md:order-2">
+      <img
+        src="./../../assets/images/banner-img.png"
+        class="md:h-[calc(100vh-300px)] w-auto"
+        alt="Banner Image"
+      />
+    </div>
+    <div class="flex flex-col gap-5">
+      <div class="flex flex-col gap-2">
+        <p class="banner-header">
+          Boldly <span class="text-[#377DFF]">African</span>, Unapologetically
+          <span class="text-[#377DFF]">Stylish</span>.
+        </p>
+        <p class="banner-desc">Experience the vibrant spirit of African fashion, where culture meets style</p>
+      </div>
+      <div>
+        <button class="btn dark">Shop Now</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.banner-header {
+  @apply font-poppins font-medium max-sm:text-3xl sm:text-5xl md:text-6xl text-[#121212];
+}
+
+.banner-desc {
+  @apply font-normal text-xl text-[#121212];
+}
+</style>
