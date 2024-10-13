50257d8b0a3c307c10bd0ca6bec5cc38bc4d384d
floxzy001 <florencegeorge292@gmail.com>
2024-10-12 19:17:03 -0700

    updated my shop page

---
+++ src/components/Home/ShopBannerComponent.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+</script>
+<template>
+    <div class="flex justify-center items-center flex-col gap-6">
+        <div class="flex justify-center gap-4 mt-28 font-[inter] text-sm">
+            <div>Home ></div>
+            <div>Shop</div>
+        </div>
+        <div class="font-[poppins] text-5xl">Shop Page</div>
+        <div class="font-[inter] text-xl">
+              Lets design the place you always imagined
+        </div>
+    </div>
+
+</template>
+
+<style>
+*{
+    font-family: inter, poppins;
+}
+.banner{
+    background: url(./../../assets/images/Banner.jpeg) no-repeat;
+    background-size: cover;
+    width: 1120px;
+    min-height: 392px;
+    margin-left: 95px;
+    margin-right: 80px;
+}
+/* .Newsletter{
+    background: url(../Images/Newsletter.png) no-repeat;
+    background-size: cover;
+    width: 1440px;
+    height: 350px;
+}
+ .border_right{
+    height: 30px;
+    align-self: center;
+    width: 2px;
+    background: #6C7275;
+ } */
+
+</style>
---
+++ src/components/Shop/ProductRatingComponent.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+// import { defineProps } from 'vue';
+import { Star } from 'lucide-vue-next';
+
+// const props = defineProps<{
+//     rating: number
+// }>()
+</script>
+<template>
+    <div class="flex flex-row top-10"> 
+        <Star :size="16"  />
+        <Star :size="16"  />
+        <Star :size="16"  />
+        <Star :size="16"  /> 
+    </div>
+    <!-- <div class="flex">
+        <Star class="pt-1" :size="16" v-for="i in Array.from({length: rating})" key="i"/>
+
+    </div>
+     -->
+
+
+</template>
